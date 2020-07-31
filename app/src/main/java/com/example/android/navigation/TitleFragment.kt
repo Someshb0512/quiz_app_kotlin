5e5a9227ff4a1a15d5bfa51db6012a94099cad86
Somesh Bachani <somesh.bachani.0512@gmail.com>
2020-08-01 02:15:37 +0530

    Added safe argument navigation in app to display score in toast.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,7 +18,7 @@ class TitleFragment : Fragment() {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater,R.layout.fragment_title,container,false)
         binding.playButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+                Navigation.createNavigateOnClickListener(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         )
         setHasOptionsMenu(true)
         return  binding.root
